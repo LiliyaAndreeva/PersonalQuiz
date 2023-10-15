26e20f3cb1edd32cfea50ba63daef6311367fda3
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-10-15 21:29:00 +0500

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 07.08.2023.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+
+    @IBOutlet weak var youAreAnimalLabel: UILabel!
+    @IBOutlet weak var animalDescribeLabel: UILabel!
+    
+    var answersChosen: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        findAnimal()
+    }
+    
+  @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+      dismiss(animated: true)
+  }
+    
+    deinit {
+        print("\(type(of: self)) has been deallocated")
+    }
+}
+
+extension ResultViewController {
+    private func findAnimal() {
+          
+    answersChosen.forEach {_ in
+          
+          let doubles = Dictionary(grouping: answersChosen, by: {$0.animal}).filter
+                        { $1.count > 1 ? true : false }.keys
+          
+          if !doubles.isEmpty {
+              
+              let animalCharacthers = (doubles.first?.rawValue)!
+              
+              switch animalCharacthers {
+              case "🐶":
+                  getAnimal(animal: animalCharacthers, describe: Animal.dog.definition)
+              case "🐱":
+                  getAnimal(animal: animalCharacthers, describe: Animal.dog.definition)
+              case "🐰":
+                  getAnimal(animal: animalCharacthers, describe: Animal.dog.definition)
+              default:
+                  getAnimal(animal: animalCharacthers, describe: Animal.dog.definition)
+              }
+          } else {
+              getAnimal(animal: "🐙", describe: "Вы - осьминог! Пройдите тест заново 😉")
+          }
+      }
+  }
+  
+  private func getAnimal(animal: Character, describe: String) {
+      youAreAnimalLabel.text = "Вы - \(animal)!"
+      animalDescribeLabel.text = describe
+  }
+}
